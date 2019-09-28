991aab45dc30574c5e4a3be4f859c5cd9d2963ac
Spectra <pbldmngz@users.noreply.github.com>
2019-09-28 13:26:30 -0700

    Create test.java

---
+++ kanter/Clase#1/test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main (String[] args) {
+      ejer1(9);
+    }
+
+    public static void ejer1 (int dim){
+        String asterisco="";
+        for(int i=1;i<=7;i++){
+            asterisco=asterisco+"*";
+            System.out.println(asterisco);
+        }
+        
+    }
+}
